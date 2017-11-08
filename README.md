bc3927e76b164710aa8a89102840fe490bf633e2
zwqStella <zwqStella@hotmail.com>
2017-11-08 13:50:52 +0800

    add codes add dependency jackson add domain class Animal

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a client for Resteasy-example-zoo
---
+++ src/main/Animal.java
@@ -0,0 +1,48 @@
+package main;
+
+public class Animal {
+   private String name;
+   private String kind;
+   private int id;
+   
+   public Animal() {
+	super();
+   }
+      
+   public Animal(String name, String kind) {
+	   this.id = -1;
+	   this.name = name;
+	   this.kind = kind;
+   }
+
+   public String getName() {
+	   return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getKind() {
+	   return kind;
+   }
+
+   public int getId() {
+	   return id;
+   }
+
+   public void setId(int id) {
+	   this.id = id;
+   }
+   
+   public void setKind(String kind) {
+	this.kind = kind;
+   }
+
+   @Override
+   public String toString() {
+	   return "Animal: id="+this.id 
+			   + ", name=" + this.name + ", kind=" + this.kind;
+   }
+   
+}
